bb7c1fd5a2eeb611e9e0d13cd6d5bde45b51fbd7
Cole Sweet <cosweet01@gmail.com>
2024-09-08 10:24:15 -0400

    Learning AGS

--- ags/config.js
+++ ags/config.js
@@ -1,26 +1,45 @@
-const time = Variable('', {
-    poll: [1000, function() {
-        return Date().toString()
-    }],
+// function Bar(monitor = 0) {
+//     const myLabel = Widget.Label({
+//         label: 'some example content',
+//     })
+
+//     Utils.interval(1000, () => {
+//         myLabel.label = Utils.exec('date')
+//     })
+
+//     return Widget.Window({
+//         monitor,
+//         name: `bar${monitor}`,
+//         anchor: ['top', 'left', 'right'],
+//         child: myLabel,
+//     })
+// }
+
+
+export {} // needed by below line
+const battery = await Service.import('battery')
+
+const date = Variable('', {
+    poll: [1000, 'date'],
 })
 
-const Bar = (/** @type {number} */ monitor) => Widget.Window({
-    monitor,
-    name: `bar${monitor}`,
-    anchor: ['top', 'left', 'right'],
-    exclusivity: 'exclusive',
-    child: Widget.CenterBox({
-        start_widget: Widget.Label({
-            hpack: 'center',
-            label: 'Welcome to AGS!',
-        }),
-        end_widget: Widget.Label({
-            hpack: 'center',
-            label: time.bind(),
-        }),
+
+const batteryProgress = Widget.CircularProgress({
+    value: battery.bind('percent').as(p => p / 100),
+    child: Widget.Icon({
+        icon: battery.bind('icon_name'),
     }),
 })
 
+const Bar = () => Widget.Window({
+    name: 'bar',
+    anchor: ['top', 'left', 'right'],
+    child: Widget.Label({ label: date.bind() })
+    // child: batteryProgress
+})
+
 App.config({
-    windows: [Bar(0)],
+    windows: [
+        Bar(),
+    ],
 })
